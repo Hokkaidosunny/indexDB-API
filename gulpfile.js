364bbf303fbd5b95664c920c0178a6b85d50c4d8
shenshuaijia <591915419@qq.com>
2016-10-09 15:44:56 +0800

    use gulp-tasks

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var tasks = require('gulp-tasks');//自己本地的包，未发布
+
+gulp.task('rollup', function() {
+  tasks.rollup_task({
+    src: './lib/index.js',
+    dest: './dist/indexdb.bundle.js',
+    moduleName: 'IndexDB'
+  });
+});
