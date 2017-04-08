27917ec82787c0cd1d5f9f3635c9387e6a14996e
ShenShuaijia <591915419@qq.com>
2017-04-08 15:10:15 +0800

    package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,49 @@
-var gulp = require('gulp');
-var tasks = require('gulp-tasks');//自己本地的包，未发布
+const gulp = require('gulp');
+const rollup = require('rollup');
+const babel = require('rollup-plugin-babel');
+const commonjs = require('rollup-plugin-commonjs');
+const nodeResolve = require('rollup-plugin-node-resolve');
+const builtins = require('rollup-plugin-node-builtins');
+
+const opts = {
+  src: './lib/index.js',
+  dest: './dist/indexdb.bundle.js',
+  moduleName: 'IndexDB',
+  babel_opts: {
+    exclude: 'node_modules/**',
+    presets: [
+      "stage-0",
+      ["es2015", {
+        "modules": false
+      }]
+    ],
+    plugins: [
+      "external-helpers"
+    ]
+  }
+};
 
 gulp.task('rollup', function() {
-  tasks.rollup_task({
-    src: './lib/index.js',
-    dest: './dist/indexdb.bundle.js',
-    moduleName: 'IndexDB'
+  rollup.rollup({
+    entry: opts.src,
+    plugins: [
+      builtins(),
+      nodeResolve({
+        jsnext: true,
+        main: true,
+        browser: true
+      }),
+      commonjs(),
+      babel(opts.babel_opts)
+    ]
+  }).then(function (bundle) {
+    return bundle.write({
+      dest: opts.dest,
+      moduleName: opts.moduleName,
+      format: "umd"
+    });
+  }).catch(function(e) {
+    console.error(e);
   });
 });
 
